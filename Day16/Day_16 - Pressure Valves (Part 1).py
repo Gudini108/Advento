5ec16dde39f78ea00caa5d926eb4565ab3f6538c
KEKS <gudini108@gmail.com>
2022-12-18 16:56:21 +0300

    Part 1 in Days 16 and 17

---
+++ Day16/Day_16 - Pressure Valves (Part 1).py
@@ -0,0 +1,73 @@
+import os
+import sys
+
+with open(os.path.join(sys. path[0], "input.txt"), "r") as f:
+    valves = f.read().split('\n')
+    
+import time
+    
+
+valves_n_tunnels = {}
+
+for line in valves:
+    
+    valve_name = line.split(' ')[1]
+    pressure_rate = line.split(' ')[4].split(';')[0].split('=')[1]
+    tunnels = line.split('valv')[1].split(' ')[1:]
+    real_tunnels = []
+    if len(tunnels) > 1:
+        for tunnel in tunnels:
+            real_tunnels.append(tunnel.split(',')[0])
+    else:
+        real_tunnels = tunnels
+    
+    valves_n_tunnels[valve_name] = {'pressure': int(pressure_rate), 'tunnels': real_tunnels}
+    
+            
+vent_copy = valves_n_tunnels.copy()
+
+closed_values = set()
+
+for tunnel in vent_copy:
+    if vent_copy[tunnel]['pressure'] > 0:
+        closed_values.add(tunnel)
+        
+
+memo = {}
+
+elememo = {}
+
+
+def pathfinder(valve_system, starting_valve, countdown, closed_valves):
+    
+    current_tunnel = valve_system[starting_valve]
+    
+    key = '/'.join([starting_valve, str(countdown), str(closed_valves)])
+    
+    if key in memo:
+        return memo[key]
+    
+    # print('closed values:', closed_valves, 'key:', key)
+    
+    if countdown == 0 or len(closed_valves) == 0:
+        return 0
+    
+    best_tunnel_result = 0
+    
+    for tunnel in current_tunnel['tunnels']:
+        copycats = closed_valves.copy()
+        best_tunnel_result = max(pathfinder(valve_system, tunnel, countdown - 1, copycats), best_tunnel_result)
+       
+    if (current_tunnel['pressure'] > 0) and (starting_valve in closed_valves):
+        copycats = closed_valves.copy()
+        copycats.remove(starting_valve)
+        result = pathfinder(valve_system, starting_valve, countdown - 1, copycats) + (countdown - 1) * current_tunnel['pressure']
+        best_tunnel_result = max(result, best_tunnel_result)
+        
+    memo[key] = best_tunnel_result
+    
+    return best_tunnel_result
+
+
+
+print( pathfinder(vent_copy, 'AA', 30, closed_values) )
---
+++ Day17/Day_17 - Falling Rocks (Part 1).py
@@ -0,0 +1,168 @@
+import os
+import sys
+
+with open(os.path.join(sys. path[0], "input.txt"), "r") as f:
+    wind_directions = list(f.read())
+    
+import time
+
+current_floor_row = -1
+floor_width = 7
+left_wall = -1
+right_wall = 7
+
+shapes = ['vertline', 'cross', 'L-shape', 'horline', 'square']
+
+settled_rocks = set()
+
+for i in range(floor_width):
+    settled_rocks.add('/'.join(str(x) for x in [i, -1]))
+
+rocks_required = 1000000000000
+
+def get_shape(shape): # get several [x, y] coordinates for different shapes
+    
+    if shape == 'vertline':
+        shape_coords = [
+            [left_wall + 3, current_floor_row + 4], 
+            [left_wall + 4, current_floor_row + 4],
+            [left_wall + 5, current_floor_row + 4],
+            [left_wall + 6, current_floor_row + 4]]
+    
+    if shape == 'cross':
+        shape_coords = [
+            [left_wall + 3, current_floor_row + 5], # left part
+            [left_wall + 4, current_floor_row + 4], # lower part of the cross
+            [left_wall + 5, current_floor_row + 5], # right part
+            [left_wall + 4, current_floor_row + 6]] # upper part of the cross
+    
+    if shape == 'L-shape':
+        shape_coords = [
+            [left_wall + 3, current_floor_row + 4], # outer left
+            [left_wall + 4, current_floor_row + 4], # second left 
+            [left_wall + 5, current_floor_row + 4], # angle
+            [left_wall + 5, current_floor_row + 5], # angle up
+            [left_wall + 5, current_floor_row + 6]] # upper part
+        
+    if shape == 'horline':
+        shape_coords = [
+            [left_wall + 3, current_floor_row + 4], 
+            [left_wall + 3, current_floor_row + 5],
+            [left_wall + 3, current_floor_row + 6],
+            [left_wall + 3, current_floor_row + 7]] # upper part
+        
+    if shape == 'square':
+        shape_coords = [
+            [left_wall + 3, current_floor_row + 4], # left down
+            [left_wall + 4, current_floor_row + 4], # right down
+            [left_wall + 3, current_floor_row + 5], # left up
+            [left_wall + 4, current_floor_row + 5]] # right up
+    
+    return shape_coords
+    
+
+shape_id = 0
+for rock in range(rocks_required):
+    rock_shape = get_shape(shapes[shape_id])
+    
+    #print('Step 1 - rock appeared', shapes[shape_id], rock_shape, current_floor_row)
+    
+    for direction in wind_directions:
+            
+        if direction == '<': # if wind blows to the left
+            shape_check = 0
+            is_blocked = False
+            
+            while shape_check != len(rock_shape):
+                
+                for seg in range(len(rock_shape)):
+                    segment = rock_shape[seg]
+                    
+                    if ((segment[0]-1 != left_wall) 
+                        and (('/'.join(str(x) for x in [segment[0]-1, segment[1]])) not in settled_rocks)):
+                        
+                        shape_check += 1
+                        
+                    else:
+                        shape_check += 1
+                        is_blocked = True
+                        
+            
+            if is_blocked == False:
+                for seg in range(len(rock_shape)):
+                    rock_shape[seg][0] -= 1
+            
+            #print('Step 2 - rock is pushed', shapes[shape_id], rock_shape, current_floor_row, direction)
+                    
+        
+        else: # if wind blows to the right
+            shape_check = 0
+            is_blocked = False
+            
+            while shape_check != len(rock_shape):
+                
+                for seg in range(len(rock_shape)):
+                    segment = rock_shape[seg]
+                    
+                    if ((segment[0]+1 != right_wall) 
+                        and ('/'.join(str(x) for x in [segment[0]+1, segment[1]]) not in settled_rocks)):
+                        
+                        shape_check += 1
+                        
+                    else:
+                        shape_check += 1
+                        is_blocked = True
+
+            
+            if is_blocked == False:
+                for seg in range(len(rock_shape)):
+                    rock_shape[seg][0] += 1
+            
+        
+        wind_directions = wind_directions[::-1]
+        wind_switcheroo = wind_directions.pop()
+        wind_directions = wind_directions[::-1]
+        wind_directions.append(wind_switcheroo)
+        
+        
+        # check if we can go down further or is it time to settle
+        shape_check = 0
+        is_blocked = False
+        while shape_check != len(rock_shape):
+            
+            for seg in range(len(rock_shape)):
+                segment = rock_shape[seg]
+                
+                if '/'.join(str(x) for x in [segment[0], segment[1]-1]) not in settled_rocks:
+                    shape_check += 1
+                    
+                else:
+                    shape_check += 1
+                    is_blocked = True
+    
+        
+        if is_blocked == False: # if we are not blocked by anything down the road - advance further
+            for seg in range(len(rock_shape)):
+                rock_shape[seg][1] -= 1
+    
+        
+        else: # if we are blocked by anything - settle instead
+            
+            if rock_shape[-1][1] > current_floor_row:
+                current_floor_row = rock_shape[-1][1]
+            
+            is_settled = True
+            
+            for seg in range(len(rock_shape)): # add our coordinated to the database
+                segment = rock_shape[seg]
+                settled_rocks.add('/'.join(str(x) for x in [segment[0], segment[1]]))
+            break
+                
+        
+    # advance our shapes to the next one
+    if shape_id == 4:
+        shape_id = 0
+    else:
+        shape_id += 1
+    
+print(current_floor_row + 1)
