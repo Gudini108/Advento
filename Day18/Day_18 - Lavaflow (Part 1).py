102aaf24f5042e8d26b047403e1ee9d8f5f202b1
KEKS <gudini108@gmail.com>
2022-12-18 23:06:45 +0300

    Minor changes

--- Day18/Day_18 - Lavaflow (Part 1).py
+++ Day18/Day_18 - Lavaflow (Part 1).py
@@ -12,25 +12,30 @@ for line in magma_droplets:
 magma_coords = []
 for line in magma_droplets:
     magma_coords.append([int(x) for x in line.split(',')])
-        
-
-sidecounter = 0
-for cube in magma_coords:
-    x, y, z = cube[0], cube[1], cube[2]
+    
+    
+def lookaround(position):
+    x, y, z = position[0], position[1], position[2]
     
     x_pos = '/'.join(str(i) for i in [x+1, y, z])
     x_neg = '/'.join(str(i) for i in [x-1, y, z])
      
-    y_pos = '/'.join(str(i) for i in [x, y-1, z])
-    y_neg = '/'.join(str(i) for i in [x, y+1, z])
+    y_pos = '/'.join(str(i) for i in [x, y+1, z])
+    y_neg = '/'.join(str(i) for i in [x, y-1, z])
     
     z_pos = '/'.join(str(i) for i in [x, y, z+1])
     z_neg = '/'.join(str(i) for i in [x, y, z-1])
     
-    neighbours = (x_pos, x_neg, y_pos, y_neg, z_pos, z_neg)
+    return (x_pos, x_neg, y_pos, y_neg, z_pos, z_neg)
+        
+
+sidecounter = 0
+for cube in magma_coords:
+    position = [cube[0], cube[1], cube[2]]
     
-    for pos in neighbours:
+    for pos in lookaround(position):
         if pos not in magma_set:
             sidecounter += 1
+
 
 print(sidecounter)
